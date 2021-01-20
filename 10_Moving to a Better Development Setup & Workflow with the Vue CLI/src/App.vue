2fa5673809934430312c4c5f9f77abce3fe3eb20
dribeirodev <diegoribeirodeveloper@gmail.com>
2021-01-20 17:55:43 -0300

    Creating a Basic Vue App

---
+++ 10_Moving to a Better Development Setup & Workflow with the Vue CLI/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id:'diego',
+          name: 'Diego Souza',
+          phone: '1234 5678 90',
+          email: 'aksjas@localhost.com'
+        },
+        {
+          id:'ana',
+          name: 'Ana Paula',
+          phone: '2222 6666 78',
+          email: 'paula@dsfg.com'
+        }
+      ]
+    }
+  },
+}
+</script>
