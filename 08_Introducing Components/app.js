8db8b09870c2e2e6d67fba36834a9c82f3060f83
dribeirodev <diegoribeirodeveloper@gmail.com>
2021-01-20 11:03:20 -0300

    Introducing Components

--- 08_Introducing Components/app.js
+++ 08_Introducing Components/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "diego",
@@ -16,6 +15,30 @@ const app = Vue.createApp({
           email: "paulasd@dskd.com",
         },
       ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">{{ visibleToggle }}</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "diego",
+        name: "Diego Ribeiro",
+        phone: "11 1 2345 6789",
+        email: "asdkj@dskd.com",
+      },
     };
   },
   computed: {
