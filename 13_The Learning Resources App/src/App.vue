f582ec3f084d0e40805b35bc4fb646bccf28a02e
dribeirodev <diegoribeirodeveloper@gmail.com>
2021-01-23 23:30:53 -0300

    Styling and More Components

--- 13_The Learning Resources App/src/App.vue
+++ 13_The Learning Resources App/src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <ul>
-    <LearningResources v-for="(res) in storedResources" :key="res.id" :res="res">
-
-    </LearningResources>
-  </ul>
+  <StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import LearningResources from './components/learning-resources/LearningResources.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
-    LearningResources
+    StoredResources
   },
   data() {
     return {
@@ -33,3 +29,19 @@ export default {
   }, 
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ 13_The Learning Resources App/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <LearningResources 
+      v-for="(res) in resources" 
+      :key="res.id" 
+      :res="res" />
+  </ul>
+</template>
+
+<script>
+import LearningResources from './LearningResources.vue';
+
+export default {
+  components: {
+    LearningResources
+  },
+  props: ['resources']
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
