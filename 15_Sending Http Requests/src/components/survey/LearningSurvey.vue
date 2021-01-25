879af8db5bb0b58fea34af305c7e206266f61344
dribeirodev <diegoribeirodeveloper@gmail.com>
2021-01-25 08:30:17 -0300

    Handling Error Responses

--- 15_Sending Http Requests/src/components/survey/LearningSurvey.vue
+++ 15_Sending Http Requests/src/components/survey/LearningSurvey.vue
@@ -28,7 +28,9 @@
         </div>
         <p
           v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        >One or more input fields are invalid. Please check your provided data.
+        </p>
+        <p v-if="error">{{error}}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -44,6 +46,7 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
   // emits: ['survey-submit'],
@@ -60,6 +63,7 @@ export default {
       //   rating: this.chosenRating,
       // });
 
+      this.error = null;
       fetch('https://vue-http-demo-98752-default-rtdb.firebaseio.com/surveys.json', {
         method: 'POST',
         headers: {
@@ -69,6 +73,15 @@ export default {
           name: this.enteredName,
           rating: this.chosenRating
         }),
+      }).then((response) => {
+        if(response.ok){
+          // 
+        } else {
+          throw new Error('Cloud not save data!');
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.error = error.message;
       });
 
       this.enteredName = '';
