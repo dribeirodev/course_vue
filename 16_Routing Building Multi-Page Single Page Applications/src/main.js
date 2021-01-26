f43925034b3de8fb59844ce83161b9d3ce83c9f1
dribeirodev <diegoribeirodeveloper@gmail.com>
2021-01-26 15:12:07 -0300

    Redirecting & "Catch All" Routes

--- 16_Routing Building Multi-Page Single Page Applications/src/main.js
+++ 16_Routing Building Multi-Page Single Page Applications/src/main.js
@@ -5,13 +5,20 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
+import NotFound from './components/nav/NotFound';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    { path: '/', redirect: '/teams' },
+    {
+      path: '/teams',
+      component: TeamsList
+      // alias: '/'
+    },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true }
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
 });
