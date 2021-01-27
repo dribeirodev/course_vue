b2719e9aeba31f0adb6d404a1cc4205967533062
dribeirodev <diegoribeirodeveloper@gmail.com>
2021-01-27 00:06:19 -0300

    Organizing Route Files

---
+++ 16_Routing Building Multi-Page Single Page Applications/src/router.js
@@ -0,0 +1,73 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './pages/TeamsList';
+import UsersList from './pages/UsersList';
+import TeamMembers from './components/teams/TeamMembers';
+import NotFound from './pages/NotFound';
+import TeamsFooter from './pages/TeamsFooter';
+import UsersFooter from './pages/UsersFooter';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      // alias: '/'
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        } // /teams/t1
+      ]
+    },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        console.log('Users beforeEnter');
+        console.log(to, from);
+        next();
+      }
+    },
+    { path: '/:notFound(.*)', component: NotFound }
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(_, _2, savedPosition) {
+    // console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  }
+});
+
+router.beforeEach(function(to, from, next) {
+  console.log('Global beforeEach');
+  console.log(to, from);
+  if (to.meta.needsAuth) {
+    console.log('Needs auth!');
+    next();
+  } else {
+    next();
+  }
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
+});
+
+router.afterEach(function(to, from) {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
+export default router;
